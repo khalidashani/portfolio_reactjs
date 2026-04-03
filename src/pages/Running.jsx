2eeceeb5a4d7d64686f81c68b9c4d4ab9408235b
khalidashani <khalidnoorashani@gmail.com>
2026-04-03 23:20:07 +0800

    minor changes to the sizing of progress bar

--- src/pages/Running.jsx
+++ src/pages/Running.jsx
@@ -63,7 +63,7 @@ export default function Running() {
         }
 
         .progress-card {
-          width: 100%;
+          width: 70%;
           background: rgba(255, 255, 255, 0.05);
           border: 1px solid rgba(255, 255, 255, 0.1);
           border-radius: 16px;
